f23839fd89cfe7e37c20b9b92d7f6faa1d222d24
Matteo <matteo.kolyszko@polito.it>
2026-05-19 11:49:24 +0200

    modified main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     data_dir = "/home/acp/datasets/SSD1/31bands_h5"
     led_path = "/home/acp/Documenti/Thouslite5.mat"
     camera_path = "/home/acp/Documenti/Sony-ILCE-6100.csv"
-    checkpoint_dir = "./run_13_sony"
+    checkpoint_dir = "./run_14_new_ill"
 
     os.makedirs(checkpoint_dir, exist_ok=True)
 
